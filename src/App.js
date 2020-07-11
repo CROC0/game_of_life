d258aaff205839dfcc47084d9b527b970005b0ff
Matt Taylor <matttaylor89@live.com.au>
2020-07-11 08:10:51 +0800

    updated icon and a few minor bug fixes

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const operations = [
 const speeds = {
   slow: 1000,
   medium: 500,
-  fast: 200,
+  fast: 100,
 };
 
 const boxSizes = {
@@ -90,6 +90,23 @@ const App = () => {
     return rows;
   };
 
+  const handleChange = (e, callback) => {
+    let value = e.target.value;
+
+    if (value.length > 1 && value[0] == '0') {
+      callback(value.slice(1));
+      return;
+    }
+
+    if (value === '') {
+      callback(0);
+    } else if (value <= 100) {
+      callback(parseInt(value));
+    } else {
+      callback(100);
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setGridWidth(numCols);
@@ -102,144 +119,139 @@ const App = () => {
     <Segment className='app'>
       <Image src={logo} style={{ margin: 'auto', width: '50%' }} />
 
-      <Grid columns={2} centered doubling>
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <Segment>
-              <div style={{ margin: '10px auto', width: '100%' }}>
-                <Button.Group vertical labeled icon>
-                  <Button
-                    icon={running ? 'pause' : 'play'}
-                    onClick={() => {
-                      setRunning(!running);
-                      if (!running) {
-                        runningRef.current = true;
-                        runSimulation();
-                      }
-                    }}
-                    color={running ? 'red' : 'blue'}
-                    content={running ? 'Stop' : 'Start'}
-                  />
-                  <Button
-                    icon='random'
-                    color='green'
-                    onClick={() => setGrid(generateRandomGrid())}
-                    content='Random'
-                  />
-                  <Button
-                    icon='dont'
-                    onClick={() => setGrid(generateEmptyGrid())}
-                    content='Clear'
-                  />
-                </Button.Group>
-              </div>
-              <div style={{ margin: '10px auto', width: '100%' }}>
-                <Popup
-                  content='Select size of cells'
-                  position='right center'
-                  trigger={
-                    <Button.Group vertical>
-                      <Button
-                        active={boxSize === boxSizes.small}
-                        onClick={() => setBoxSize(boxSizes.small)}>
-                        Small
-                      </Button>
-                      <Button
-                        active={boxSize === boxSizes.large}
-                        onClick={() => setBoxSize(boxSizes.large)}>
-                        Large
-                      </Button>
-                    </Button.Group>
-                  }
+      <Grid columns={2} stackable reversed='mobile vertically'>
+        <Grid.Column width={3}>
+          <Segment>
+            <div style={{ margin: '10px auto', width: '100%' }}>
+              <Button.Group vertical labeled icon>
+                <Button
+                  icon={running ? 'pause' : 'play'}
+                  onClick={() => {
+                    setRunning(!running);
+                    if (!running) {
+                      runningRef.current = true;
+                      runSimulation();
+                    }
+                  }}
+                  color={running ? 'red' : 'blue'}
+                  content={running ? 'Stop' : 'Start'}
                 />
-              </div>
-              <div style={{ margin: '10px auto', width: '100%' }}>
-                <Popup
-                  content='Select speed of generations'
-                  position='right center'
-                  trigger={
-                    <Button.Group vertical>
-                      <Button active={speed === speeds.slow} onClick={() => setSpeed(speeds.slow)}>
-                        Slow
-                      </Button>
-                      <Button
-                        active={speed === speeds.medium}
-                        onClick={() => setSpeed(speeds.medium)}>
-                        Medium
-                      </Button>
-                      <Button active={speed === speeds.fast} onClick={() => setSpeed(speeds.fast)}>
-                        Fast
-                      </Button>
-                    </Button.Group>
-                  }
+                <Button
+                  icon='random'
+                  color='green'
+                  onClick={() => setGrid(generateRandomGrid())}
+                  content='Random'
                 />
-              </div>
-              <Form onSubmit={handleSubmit}>
-                <Popup
-                  content='Maximum size is 100'
-                  position='right center'
-                  trigger={
-                    <Form.Field>
-                      <label>Number of Rows</label>
-                      <input
-                        type='number'
-                        placeholder='Default is 30'
-                        value={numRows}
-                        onChange={(e) => setNumRows(Math.min(parseInt(e.target.value), 100))}
-                      />
-                    </Form.Field>
-                  }
-                />
-                <Popup
-                  content='Maximum size is 100'
-                  position='right center'
-                  trigger={
-                    <Form.Field>
-                      <label>Number of Columns</label>
-                      <input
-                        type='number'
-                        placeholder='Default is 30'
-                        value={numCols}
-                        onChange={(e) => setNumCols(Math.min(parseInt(e.target.value), 100))}
-                      />
-                    </Form.Field>
-                  }
-                />
-
-                <Button type='submit' loading={loading}>
-                  Update Grid
-                </Button>
-                {loading && 'loading'}
-              </Form>
-            </Segment>
-          </Grid.Column>
-          <Grid.Column width={13}>
-            <div
-              style={{
-                display: 'grid',
-                gridTemplateColumns: `repeat(${gridWidth}, ${boxSize}px)`,
-              }}>
-              {grid.map((row, i) =>
-                row.map((col, k) => (
-                  <div
-                    onClick={() => {
-                      const newGrid = produce(grid, (gridCopy) => {
-                        gridCopy[i][k] = grid[i][k] ? 0 : 1;
-                      });
-                      setGrid(newGrid);
-                    }}
-                    key={`${i}-${k}`}
-                    style={{
-                      height: boxSize,
-                      width: boxSize,
-                      border: '1px solid black',
-                      backgroundColor: grid[i][k] ? 'pink' : null,
-                    }}></div>
-                ))
-              )}
+                <Button icon='dont' onClick={() => setGrid(generateEmptyGrid())} content='Clear' />
+              </Button.Group>
             </div>
-          </Grid.Column>
-        </Grid.Row>
+            <div style={{ margin: '10px auto', width: '100%' }}>
+              <Popup
+                content='Select size of cells'
+                position='right center'
+                trigger={
+                  <Button.Group vertical>
+                    <Button
+                      active={boxSize === boxSizes.small}
+                      onClick={() => setBoxSize(boxSizes.small)}>
+                      Small
+                    </Button>
+                    <Button
+                      active={boxSize === boxSizes.large}
+                      onClick={() => setBoxSize(boxSizes.large)}>
+                      Large
+                    </Button>
+                  </Button.Group>
+                }
+              />
+            </div>
+            <div style={{ margin: '10px auto', width: '100%' }}>
+              <Popup
+                content='Select speed of generations'
+                position='right center'
+                trigger={
+                  <Button.Group vertical>
+                    <Button active={speed === speeds.slow} onClick={() => setSpeed(speeds.slow)}>
+                      Slow
+                    </Button>
+                    <Button
+                      active={speed === speeds.medium}
+                      onClick={() => setSpeed(speeds.medium)}>
+                      Medium
+                    </Button>
+                    <Button active={speed === speeds.fast} onClick={() => setSpeed(speeds.fast)}>
+                      Fast
+                    </Button>
+                  </Button.Group>
+                }
+              />
+            </div>
+            <Form onSubmit={handleSubmit}>
+              <Popup
+                content='Maximum size is 100'
+                position='right center'
+                trigger={
+                  <Form.Field>
+                    <label>Number of Rows</label>
+                    <input
+                      type='number'
+                      placeholder='Default is 30'
+                      value={numRows}
+                      onChange={(e) => handleChange(e, setNumRows)}
+                      max={100}
+                    />
+                  </Form.Field>
+                }
+              />
+              <Popup
+                content='Maximum size is 100'
+                position='right center'
+                trigger={
+                  <Form.Field>
+                    <label>Number of Columns</label>
+                    <input
+                      type='number'
+                      placeholder='Default is 30'
+                      value={numCols}
+                      onChange={(e) => handleChange(e, setNumCols)}
+                    />
+                  </Form.Field>
+                }
+              />
+
+              <Button type='submit' loading={loading}>
+                Update Grid
+              </Button>
+              {loading && 'loading'}
+            </Form>
+          </Segment>
+        </Grid.Column>
+        <Grid.Column width={13}>
+          <div
+            style={{
+              display: 'grid',
+              gridTemplateColumns: `repeat(${gridWidth}, ${boxSize}px)`,
+            }}>
+            {grid.map((row, i) =>
+              row.map((col, k) => (
+                <div
+                  onClick={() => {
+                    const newGrid = produce(grid, (gridCopy) => {
+                      gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                    });
+                    setGrid(newGrid);
+                  }}
+                  key={`${i}-${k}`}
+                  style={{
+                    height: boxSize,
+                    width: boxSize,
+                    border: '1px solid black',
+                    backgroundColor: grid[i][k] ? 'pink' : null,
+                  }}></div>
+              ))
+            )}
+          </div>
+        </Grid.Column>
       </Grid>
       <Help />
     </Segment>
